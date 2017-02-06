317ce9421c504903f3f2d3119012abaf9b5aa38d
Tom Kuijer <tom@flooris.nl>
2017-02-06 12:36:25 +0100

    Initial commit

---
+++ HrefLangTags.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Statamic\Addons\HrefLang;
+
+use Statamic\API\Content;
+use Statamic\Extend\Tags;
+
+class HrefLangTags extends Tags
+{
+    /**
+     * The {{ href_lang }} tag
+     *
+     * @return string|array
+     */
+    public function index()
+    {
+        $page = $this->context['id'];
+
+        /** @var \Statamic\Data\Content\Content $data */
+        $data = Content::find($page);
+
+        $locales = $data->locales();
+
+        $alternates = [];
+
+        foreach($locales as $locale) {
+            $data->locale($locale);
+            $localized_uri = $data->absoluteUrl();
+
+            $alternates[] = [
+                'lang' => $locale,
+                'url' => $localized_uri,
+            ];
+
+        }
+
+        return $this->parseLoop($alternates);
+    }
+}
